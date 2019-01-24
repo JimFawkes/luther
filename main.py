4ba98e78d4fb7e36b84f51436cacd6f740a364d2
Jim Fawkes <meilfort@protonmail.com>
2019-01-23 23:31:10 -0800

    Make main executable

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from loguru import logger
 logger.level("RESULTS", no=40, color="<green>")
 _log_file_name = __file__.split("/")[-1].split(".")[0]
 logger.add(f"logs/{_log_file_name}.log", rotation="1 day")
-logger.add(f"logs/success_{_log_file_name}.log", level='SUCCESS')
+logger.add(f"logs/success_{_log_file_name}.log", level="SUCCESS")
 logger.add(f"logs/success.log", rotation="1 day", level="SUCCESS")
+
 
 def main():
     """Run the entire pipeline.
@@ -20,3 +21,7 @@ def main():
     modeling.validate_all(training, validation)
     logger.success(f"Finished Modeling and Validating")
     logger.success(f"Finished Pipeline")
+
+
+if __name__ == "__main__":
+    main()
